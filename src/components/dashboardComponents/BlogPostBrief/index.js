2572a21738da62f0a3f9ae252a1bd6050876844b
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-10 17:57:04 -0800

    made post brief containers collapsable

--- src/components/dashboardComponents/BlogPostBrief/index.js
+++ src/components/dashboardComponents/BlogPostBrief/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 export default function BlogPostBrief(props) {
+    const [isExpanded, setIsExpanded] = useState(false)
+
     return (
-        <div className='my-post-brief'>
+        <div className={isExpanded ? 'my-post-brief expanded' : 'my-post-brief'}>
             <div className='my-post-details'>
                 <a className='my-post-title' href={`/post/${props.id}`} target='_blank'><strong>{props.title}</strong></a>
                 <p className='my-post-description'>{props.description}</p>
@@ -12,6 +14,7 @@ export default function BlogPostBrief(props) {
                 <button className='btn btn-primary' onClick={() => props.showPostUpdateModal(props.title, props.description, props.id)}>Details</button>
                 <button className='btn btn-primary' onClick={() => window.open('/user/' + props.userId + '/post/update/' + props.id)}>Edit</button>
                 <button className='btn btn-danger' onClick={() => props.handlePostDeleteAttempt(props.id)}>Delete</button>
+                <a href='#' onClick={() => setIsExpanded(!isExpanded)}>{isExpanded ? 'Read Less' : 'Read More'}</a>
             </div>
         </div>
     )
