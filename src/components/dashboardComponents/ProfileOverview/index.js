7d2ec01e45f4d1f69d77d2b1547a6b63e11d5d8e
Brandon Phillips <brandon.phillips976@gmail.com>
2021-01-10 18:56:04 -0800

    maded dashboard mobile responsive

--- src/components/dashboardComponents/ProfileOverview/index.js
+++ src/components/dashboardComponents/ProfileOverview/index.js
@@ -5,17 +5,19 @@ export default function ProfileOverview() {
     return (
         <div className='profile-container'>
             <h2>My Profile</h2>
-            <div className='profile-info-group'>
-                <p className='info-heading'><strong>Name</strong></p>
-                <p className='info'>Brandon</p>
-            </div>
-            <div className='profile-info-group'>
-                <p className='info-heading'><strong>Email</strong></p>
-                <p className='info'>email@email.com</p>
-            </div>
-            <div className='profile-info-group'>
-                <p className='info-heading'><strong>Bio</strong></p>
-                <p className='info'>This is my bio</p>
+            <div className='profile-flex-wrapper'>
+                <div className='profile-info-group name'>
+                    <p className='info-heading'><strong>Name</strong></p>
+                    <p className='info'>Brandon</p>
+                </div>
+                <div className='profile-info-group email'>
+                    <p className='info-heading'><strong>Email</strong></p>
+                    <p className='info'>email@email.com</p>
+                </div>
+                <div className='profile-info-group bio'>
+                    <p className='info-heading'><strong>Bio</strong></p>
+                    <p className='info'>This is my bio</p>
+                </div>
             </div>
         </div>
     )
